3fe161af69ba56fbaa43544d055a88c1812c4092
张鹏杰 <pengjie.zhang@71360.com>
2021-07-06 15:25:23 +0800

    JFinalProject 调整为 maven结构

--- JFinalProject/src/main/java/demo/DemoConfig.java
+++ JFinalProject/src/main/java/demo/DemoConfig.java
@@ -11,15 +11,20 @@ import com.jfinal.template.Engine;
 
 public class DemoConfig extends JFinalConfig{
 	
+	@Override
 	public void configConstant(Constants me) {
 		me.setDevMode(true);
 		me.setViewType(ViewType.JSP);
 	}
+	@Override
 	public void configRoute(Routes me) {
 		me.add("/hello",HelloController.class);
 	}
+	@Override
 	public void configPlugin(Plugins me) {}
+	@Override
 	public void configInterceptor(Interceptors me) {}
+	@Override
 	public void configHandler(Handlers me) {}
 	@Override
 	public void configEngine(Engine arg0) {
